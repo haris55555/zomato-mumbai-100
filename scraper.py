194eee20bb130bd8371cc866ddc61b6f9643c48a
Haris Ahmed Ahmed <108285896+haris55555@users.noreply.github.com>
2022-07-07 22:35:38 +0000

    fetched the page title

--- scraper.py
+++ scraper.py
@@ -1,14 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 zomato_mumbai_url = "https://www.zomato.com/mumbai/great-food-no-bull"
 
-chrome_options=Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usgae')
-driver = webdriver.chrome(options=chrome_options)
 
-driver.get(zomato_mumbai_url)
 
-print('page',driver.title)
+def get_driver():
+ chrome_options=Options()
+ chrome_options.add_argument('--no-sandbox')
+ chrome_options.add_argument('--headless')
+ chrome_options.add_argument('--disable-dev-shm-usgae')
+ user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
+ chrome_options.add_argument('user-agent=       {0}'.format(user_agent))
+ driver = webdriver.Chrome(options=chrome_options)
+ return driver
+
+
+if __name__ =="__main__":
+  print('creating driver')
+  driver = get_driver()
+  
+  print('fetching page')
+  driver.get(zomato_mumbai_url)
+  print('title:',driver.title)
+
